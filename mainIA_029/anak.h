125f032e0ee8fa3c9a29dd0d1a50ddf4dc699d93
RamaSandhy <rshandy07@gmail.com>
2023-05-24 17:45:38 +0700

    int main ibu dan anak

--- mainIA_029/anak.h
+++ mainIA_029/anak.h
@@ -1,1 +1,14 @@
-#pragma once
+#ifndef ANAK_H
+#define Anak_H
+
+class anak {
+public:
+	string nama;
+	anak(string pNama) :nama(pNama) {
+		cout << "Anak \"" << nama << "\" ada\n";
+	}
+	~anak() {
+		cout << "Anak \"" << nama << "\" tidak ada\n";
+	}
+};
+
--- mainIA_029/ibu.h
+++ mainIA_029/ibu.h
@@ -1,1 +1,13 @@
-ifndef 
+#ifndef IBU_H
+#define IBU_H
+
+class ibu {
+public:
+	string nama;
+	vector<anak*> daftar_anak;
+
+	ibu(string pNama) {
+		cout << "Ibu \"" << nama << "\" ada\n";
+	}
+
+};
