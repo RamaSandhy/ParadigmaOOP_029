d48dcd9b021eafa714183d02230732f645ac9534
RamaSandhy <rshandy07@gmail.com>
2023-05-24 17:57:02 +0700

    menambahkan header ibu

--- mainIA_029/ibu.h
+++ mainIA_029/ibu.h
@@ -9,5 +9,20 @@ public:
 	ibu(string pNama) {
 		cout << "Ibu \"" << nama << "\" ada\n";
 	}
-
+	~ibu() {
+		cout << "Ibu \"" << nama << "\" tidak ada\n";
+	}
+	void tambahAnak(anak* pAnak);
+	void cetakAnak();
 };
+void ibu::tambahAnak(anak* pAnak) {
+	daftar_anak.push_back(pAnak);
+}
+void ibu::cetakAnak() {
+	cout << "Daftar Anak dari Ibu \"" << this->nama << "\":\n";
+	for (auto& a : daftar_anak) {
+		cout << a->nama << "\n";
+	}
+	cout << endl;
+}
+#endif // !IBU_H
